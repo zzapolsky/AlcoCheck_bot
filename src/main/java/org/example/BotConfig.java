fe118369e36f2deae04327606b36eb0e33bdb0b3
zzapolsky <118912034+zzapolsky@users.noreply.github.com>
2023-06-16 19:40:44 +0500

    Update BotConfig.java

--- src/main/java/org/example/BotConfig.java
+++ src/main/java/org/example/BotConfig.java
@@ -3,6 +3,6 @@ package org.example;
 public class BotConfig {                  // тут содержатся токен и имя бота
 
     public static String name = "@AcloChekerBot";
-    public static String token = "6133954224:AAE-rz1CtGNUhwc7fXspcFxmSOTvWeipzas";
+    public static String token = "";
 
 }
